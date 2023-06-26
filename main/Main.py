dea2acdb9e28efd302ad86559fe74a531c1987f2
gwyeorge <105829849+gwyeorge@users.noreply.github.com>
2023-06-26 10:50:20 +0300

    Update Main.py
    added functions

--- main/Main.py
+++ main/Main.py
@@ -1,5 +1,28 @@
 from tkinter import *
 
+#finish fuctions for calculations
+#make cos,sin and tan (eventually work)
+#add ln, log, e and tan, commands for buttons ready need to fix x,y and draw the buttons
+
+"""self.log_image = PhotoImage(file="images/log.png")
+        self.b13 = Button(self.window, text='log', image=self.log_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'log('))
+        self.b13.place(x=3, y=130)
+        
+   self.ln_image = PhotoImage(file="images/ln.png")
+        self.b13 = Button(self.window, text='ln', image=self.ln_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'ln('))
+        self.b13.place(x=3, y=130)
+   
+   self.e_image = PhotoImage(file="images/e.png")
+        self.b13 = Button(self.window, text='e', image=self.e_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'e'))
+        self.b13.place(x=3, y=130)
+   
+   self.tan_image = PhotoImage(file="images/tan.png")
+        self.b13 = Button(self.window, text='tan', image=self.tan_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'tan('))
+        self.b13.place(x=3, y=130)               """
 
 class functions:
     def __init__(self, entry_widget):
@@ -8,7 +31,20 @@ class functions:
     def click(self):
         pass
 
+    def insert_character(self, character):
+        equation = self.entry_widget.get()
+        if character in ['+', '-', '*', '/'] and not equation:
+            return
+        self.entry_widget.insert(END, character)
 
+    def backspace(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def Equation(self):
+        pass
 
 class calculator:
     def __init__(self):
@@ -19,7 +55,7 @@ class calculator:
         self.operator = ""
         self.text_Input = StringVar()
         self.txtDisplay = None
-        self.txtDisplay_image = None  # Initialize txtDisplay_image as None
+        self.txtDisplay_image = None
 
         self.fun = functions(entry_widget=None)
 
@@ -45,75 +81,73 @@ class calculator:
     def general_buttons(self):
         self.backspace_image = PhotoImage(file="images/backspace.png")
         self.b1 = Button(self.window, text='bsce', image=self.backspace_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+                         command=self.fun.backspace)
         self.b1.place(x=216, y=80)
 
         self.division_image = PhotoImage(file="images/division.png")
         self.b2 = Button(self.window, text='div', image=self.division_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+                         command=lambda: self.fun.insert_character('÷'))
         self.b2.place(x=216, y=130)
 
         self.multiplication_image = PhotoImage(file="images/multiplication.png")
         self.b3 = Button(self.window, text='x', image=self.multiplication_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+                         command=lambda: self.fun.insert_character('x'))
         self.b3.place(x=216, y=180)
 
         self.subtraction_image = PhotoImage(file="images/subtraction.png")
         self.b4 = Button(self.window, text='-', image=self.subtraction_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+                         command=lambda: self.fun.insert_character('-'))
         self.b4.place(x=216, y=230)
 
         self.addition_image = PhotoImage(file="images/addition.png")
         self.b5 = Button(self.window, text='+', image=self.addition_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+                         command=lambda: self.fun.insert_character('+'))
         self.b5.place(x=216, y=280)
 
         self.equal_image = PhotoImage(file="images/equals.png")
         self.b6 = Button(self.window, text='=', image=self.equal_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
-        self.b6.place(x=216, y=330)
+                         command=lambda: self.fun.entry_widget.insert(END, '√'))
+        self.b6.place(x=145, y=330)
 
         self.clear_image = PhotoImage(file="images/clear.png")
         self.b7 = Button(self.window, text='c', image=self.clear_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+                         command=self.fun.clear)
         self.b7.place(x=145, y=80)
 
         self.root_image = PhotoImage(file="images/root.png")
-        self.b8 = Button(self.window, text='2ryza', image=self.root_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
+        self.b8 = Button(self.window, text='√', image=self.root_image, bg='gold', fg='black', font='Norasi',
+                         command=lambda: self.fun.entry_widget.insert(END, '√'))
         self.b8.place(x=145, y=130)
 
         self.dot_image = PhotoImage(file="images/dot.png")
         self.b9 = Button(self.window, text='.', image=self.dot_image, bg='gold', fg='black', font='Norasi',
-                         command=self.fun.click)
-        self.b9.place(x=74, y=330)
+                         command=lambda: self.fun.entry_widget.insert(END, '.'))
+        self.b9.place(x=3, y=330)
 
         self.square_image = PhotoImage(file="images/square2.png")
         self.b10 = Button(self.window, text='^', image=self.square_image, bg='gold', fg='black', font='Norasi',
-                          command=self.fun.click)
+                          command=lambda: self.fun.entry_widget.insert(END, '^'))
         self.b10.place(x=74, y=130)
 
         self.percent_image = PhotoImage(file="images/percent.png")
         self.b11 = Button(self.window, text='%', image=self.percent_image, bg='gold', fg='black', font='Norasi',
-                          command=self.fun.click)
+                          command=lambda: self.fun.entry_widget.insert(END, '%'))
         self.b11.place(x=74, y=80)
 
         self.sin_image = PhotoImage(file="images/sin.png")
-        self.b12 = Button(self.window, text='%', image=self.sin_image, bg='gold', fg='black', font='Norasi',
-                          command=self.fun.click)
+        self.b12 = Button(self.window, text='sin', image=self.sin_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'sin('))
         self.b12.place(x=3, y=80)
 
         self.cos_image = PhotoImage(file="images/cos.png")
-        self.b13 = Button(self.window, text='%', image=self.cos_image, bg='gold', fg='black', font='Norasi',
-                          command=self.fun.click)
+        self.b13 = Button(self.window, text='cos', image=self.cos_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'cos('))
         self.b13.place(x=3, y=130)
 
         self.pi_image = PhotoImage(file="images/pi.png")
-        self.b14 = Button(self.window, text='1', image=self.pi_image, bg='gold', fg='black', font='Norasi',
-                          command=self.fun.click)
-        self.b14.place(x=145, y=330)
-
-        # Rest of the button configurations...
+        self.b14 = Button(self.window, text='pi', image=self.pi_image, bg='gold', fg='black', font='Norasi',
+                          command=lambda: self.fun.entry_widget.insert(END, 'Π'))
+        self.b14.place(x=74, y=330)
 
     def numbers(self):
         self.fun = functions(entry_widget=self.txtDisplay)
